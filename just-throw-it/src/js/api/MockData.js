24718dbbc2abef10dd0262514edbae760e617b0d
Indrek Toomsalu <intoom@ttu.ee>
2018-02-17 14:09:32 +0200

    Added FieldPicker Cards to a grid and styled them little bit

--- just-throw-it/src/js/api/MockData.js
+++ just-throw-it/src/js/api/MockData.js
@@ -109,5 +109,97 @@ export const fields = [
                 "TrackPar": 7
             }
         ]
-    }
+    },
+	{
+		"FieldID": 6,
+		"FieldName": "Random",
+		"NumberOfTracks": 4,
+		"Tracks": [
+			{
+				"TrackNumber": 1,
+				"TrackPar": 2
+			},
+			{
+				"TrackNumber": 2,
+				"TrackPar": 3
+			},
+			{
+				"TrackNumber": 3,
+				"TrackPar": 5
+			},
+			{
+				"TrackNumber": 4,
+				"TrackPar": 7
+			}
+		]
+	},
+	{
+		"FieldID": 7,
+		"FieldName": "Random",
+		"NumberOfTracks": 4,
+		"Tracks": [
+			{
+				"TrackNumber": 1,
+				"TrackPar": 2
+			},
+			{
+				"TrackNumber": 2,
+				"TrackPar": 3
+			},
+			{
+				"TrackNumber": 3,
+				"TrackPar": 5
+			},
+			{
+				"TrackNumber": 4,
+				"TrackPar": 7
+			}
+		]
+	},
+	{
+		"FieldID": 8,
+		"FieldName": "Random",
+		"NumberOfTracks": 4,
+		"Tracks": [
+			{
+				"TrackNumber": 1,
+				"TrackPar": 2
+			},
+			{
+				"TrackNumber": 2,
+				"TrackPar": 3
+			},
+			{
+				"TrackNumber": 3,
+				"TrackPar": 5
+			},
+			{
+				"TrackNumber": 4,
+				"TrackPar": 7
+			}
+		]
+	},
+	{
+		"FieldID": 9,
+		"FieldName": "Random",
+		"NumberOfTracks": 4,
+		"Tracks": [
+			{
+				"TrackNumber": 1,
+				"TrackPar": 2
+			},
+			{
+				"TrackNumber": 2,
+				"TrackPar": 3
+			},
+			{
+				"TrackNumber": 3,
+				"TrackPar": 5
+			},
+			{
+				"TrackNumber": 4,
+				"TrackPar": 7
+			}
+		]
+	}
 ];
--- just-throw-it/src/js/field/FieldPicker.js
+++ just-throw-it/src/js/field/FieldPicker.js
@@ -1,35 +1,44 @@
 import React from 'react';
+import "../../style/FieldPicker.css";
 import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button, CardDeck
+	Card, CardImg, CardText, CardBody,
+	CardTitle, CardSubtitle, Button, CardDeck
 } from 'reactstrap';
 
 class FieldPicker extends React.Component {
 
-    renderAllFields() {
-        return this.props.fields.map(field => {
-            return (<Card key={field.FieldID}>
-                <CardImg top width="10%"
-                         src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-                         alt="Card image cap"/>
-                <CardBody>
-                    <CardTitle>{field.FieldName}</CardTitle>
-                    <CardSubtitle>Number of fields {field.NumberOfTracks}</CardSubtitle>
-                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's
-                        content.</CardText>
-                    <Button>Pick field</Button>
-                </CardBody>
-            </Card>)
-        })
-    }
+	renderAllFields() {
+		return this.props.fields.map(field => {
+			return (<div className="col-sm col-md-4">
+				<Card key={field.FieldID} className="field-card">
+					<CardImg top width="10%"
+							 src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+							 alt="Card image cap"/>
+					<CardBody>
+						<CardTitle>{field.FieldName}</CardTitle>
+						<CardSubtitle>Number of fields {field.NumberOfTracks}</CardSubtitle>
+						<CardText>Some quick example text to build on the card title and make up the bulk of the card's
+							content.</CardText>
+						<Button>Pick field</Button>
+					</CardBody>
+				</Card>
+			</div>)
+		})
+	}
 
-    render() {
-        return (
-            <CardDeck>
-                {this.renderAllFields()}
-            </CardDeck>
-        )
-    }
+	render() {
+		return (
+			<div className="container">
+				<h2>Please pick a field</h2>
+				<hr/>
+				<div className="row justify-content-center">
+					<CardDeck>
+						{this.renderAllFields()}
+					</CardDeck>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default FieldPicker;
