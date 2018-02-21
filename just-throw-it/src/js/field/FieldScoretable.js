73307641c70cc9a42b31ba506d7daeb4d1526d6a
hetoms <32268433+hetoms@users.noreply.github.com>
2018-02-21 15:50:33 +0200

    redux

--- just-throw-it/src/js/field/FieldScoretable.js
+++ just-throw-it/src/js/field/FieldScoretable.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {Collapse, Button, CardBody, Card} from 'reactstrap';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import Track from "./Track";
+import PropTypes from 'prop-types'
 
 class FieldScoretable extends React.Component {
 	constructor(props) {
 		super(props);
 		this.setTracks = this.setTracks.bind(this);
 	}
+
 
 	setTracks() {
 		let id = 1;
@@ -37,4 +39,5 @@ class FieldScoretable extends React.Component {
 }
 
 export default FieldScoretable;
+
 
