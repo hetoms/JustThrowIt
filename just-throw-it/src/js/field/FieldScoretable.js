212451926e55706e1779e64b75083e38cb07e284
Indrek Toomsalu <intoom@ttu.ee>
2018-02-22 19:46:55 +0200

    Changed the field tracks view

--- just-throw-it/src/js/field/FieldScoretable.js
+++ just-throw-it/src/js/field/FieldScoretable.js
@@ -1,34 +1,59 @@
 import React from "react";
-import {Button} from 'reactstrap';
+import {Button, Nav, NavLink, NavItem} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import Track from "./Track";
+import "../../style/FieldScoretable.css";
 import PropTypes from 'prop-types';
 
 class FieldScoretable extends React.Component {
 	constructor(props) {
 		super(props);
 		this.setTracks = this.setTracks.bind(this);
+		this.showTrack = this.showTrack.bind(this);
+
+		this.state = {
+			displayedTrack: this.props.field.tracks[0]
+		}
 	}
 
+	showTrack(track, e) {
+		e.preventDefault();
+		this.setState({
+			displayedTrack: track
+		})
+	}
 
 	setTracks() {
+		// return this.props.field.tracks.map(track => {
+		// 	return <Track track={track}/>
+		// });
+
 		return this.props.field.tracks.map(track => {
-			return <Track track={track}/>
-		});
+			return (
+				<NavItem>
+					<NavLink href="#" onClick={(e) => this.showTrack(track, e)}>Track: { track.trackNumber }</NavLink>
+				</NavItem>
+			)
+		})
 	}
 
 	render() {
-		console.log(this.props);
 		return (
 			<div className="container">
 				<div>
 					<Link to='/pickField'><Button>Back</Button></Link>
 					<h2>{this.props.field.fieldName} DiscGolf field</h2>
 				</div>
-
 				<hr/>
-				<div>
-					{this.setTracks()}
+				<div className="tracks-container">
+					<div className="tracks-nav">
+						<Nav vertical>
+							{this.setTracks()}
+						</Nav>
+					</div>
+					<div className="track-info">
+						<Track track={this.state.displayedTrack}/>
+					</div>
 				</div>
 			</div>
 		)
