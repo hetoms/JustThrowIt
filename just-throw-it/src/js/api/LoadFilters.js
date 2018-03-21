628fa7f7887c9306a998d746e78973baca81217b
hetoms <32268433+hetoms@users.noreply.github.com>
2018-03-22 00:05:51 +0200

    Connected filter to API

--- just-throw-it/src/js/api/LoadFilters.js
+++ just-throw-it/src/js/api/LoadFilters.js
@@ -1,13 +1,11 @@
-const apiURL = 'http://justthrowit-env.eu-central-1.elasticbeanstalk.com/fields';
+const apiURL = 'http://justthrowit-env.eu-central-1.elasticbeanstalk.com/counties';
 const response = ['harju', 'pärnu', 'narva', 'tartu'];
 
-const loadFilters = (callback) => {
-  /*
+const loadFilters = async (callback) => { 
   fetch(apiURL, {cache: 'no-store'})
-    .then((response) => saveAreaFilters(response))
-    */
-  console.log('wat', response);
-  callback(response);
+    .then(function (response) {
+      return response.json();
+    }).then( text => callback(text));
 };
 
 export default loadFilters;
